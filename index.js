a72ddba25463b9815bf5ed1c0ea734e23abb1034
John Payne <jjrpayne@uvic.ca>
2021-09-23 18:06:52 -0700

    added get from omdb route

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const got = require('got');
 const app = express();
 const port = process.env.PORT || 5001;
 const production = process.env.PRODUCTION || false;
-const api_key = process.env.API_KEY || null;
+const api_key = process.env.API_KEY;
+const web_app = process.env.WEBAPP;
 
 app.get('/', (req, res) => {
-  res.send('hello :)')
+	res.send('hello :)')
+});
+
+app.get('/get_hostname', (req, res) => {
+	res.send(req.hostname)
+});
+
+app.get('/get_from_omdb', (req, res) => {
+	(async () => {
+		try {
+			const response = await got(req.body.url + `&apikey=${api_key}`);
+			res.send(response.body);
+		} catch(error) {
+			res.send(error.response.body);
+		}
+	})();
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`Listening on port ${port}`)
 })
