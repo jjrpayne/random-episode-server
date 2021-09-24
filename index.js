36ff50ce3d9628fdc1dc1d899c92f41ba35010e3
John Payne <jjrpayne@uvic.ca>
2021-09-23 19:21:25 -0700

    get from omdb route

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const got = require('got');
 const app = express();
-const port = process.env.PORT || 5001;
+const got = require('got');
+const {pipeline} = require('stream');
+
+require('dotenv').config()
+
+const port = process.env.PORT || 3001;
 const production = process.env.PRODUCTION || false;
 const api_key = process.env.API_KEY;
 const web_app = process.env.WEBAPP;
+const omdb_api = "https://omdbapi.com";
 
 app.get('/', (req, res) => {
-	res.send('hello :)')
+	res.send('welcome to the random episode server :)')
 });
 
 app.get('/get_hostname', (req, res) => {
@@ -15,14 +20,21 @@ app.get('/get_hostname', (req, res) => {
 });
 
 app.get('/get_from_omdb', (req, res) => {
-	(async () => {
-		try {
-			const response = await got(req.body.url + `&apikey=${api_key}`);
-			res.send(response.body);
-		} catch(error) {
-			res.send(error.response.body);
+	// console.log(production);
+	if (production == true){
+		if(req.hostname != web_app)
+			return res.status(401).send("You are requesting this data from an unauthorized host.");
+	}
+	var params = req.query;
+	params["apikey"] = api_key;
+	// console.log(params);
+	const dataStream = got.stream(omdb_api, {searchParams: params});
+	pipeline(dataStream, res, (err) => {
+		if(err) {
+			console.log(err);
+			res.sendStatus(500);
 		}
-	})();
+	});
 });
 
 app.listen(port, () => {
